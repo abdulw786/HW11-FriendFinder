39eb9492f3e67e3c3f3599ad9cb62ceaa3ea5515
abdul wahid hashoory <whashoory@gmail.com>
2019-06-14 02:02:21 -0500

    install npms

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
-// Node Dependencies
-let express = require('express');
-let bodyParser = require('body-parser');
-let path = require('path');
+var express = require ('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-// Link in html and api routes
-let apiRoutes = require('./app/routing/api-routes.js');
-let htmlRoutes = require('./app/routing/html-routes.js');
-
-// Set up Express App
-let app = express();
-let PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.json());
+var app = express();
+var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-// Server Routing Map 
-apiRoutes(app); // API route - Must be listed first due to the HTML default catch all "use" route
-htmlRoutes(app); // HTML route 
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
-// Listener - Start the server
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
